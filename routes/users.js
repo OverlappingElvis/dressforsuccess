86f421c9f4138d2a1623123e2e7c9d8d818b691a
Jeremy Sher <OverlappingElvis@gmail.com>
2013-05-19 12:30:14 -0700

    adding methods for users

--- routes/users.js
+++ routes/users.js
@@ -20,8 +20,23 @@ exports.findById = function(req, res) {
 
 exports.findAll = function(req, res) {
 	res.send('User list goes here');
+	db.collection('users', function(err, collection) {
+		collection.find().toArray(function(err, items) {
+			res.send(items);
+		});
+	});
 };
 
 exports.createUser = function(req, res) {
-	
+	var user = req.body;
+	console.log('adding: ' + JSON.stringify(user));
+	db.collection('users', function(err, collection) {
+		db.collection.insert(user, { safe: true }, function(err, results) {
+			if (err) {
+				console.log('error adding user: ' + JSON.stringify(err));
+			} else {
+				console.log('added user: ' + JSON.stringify(results));
+				res.send(result[0]);
+		});
+	});
 };
